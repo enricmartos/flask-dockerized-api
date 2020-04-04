738489aceda94102fcea28497a8efd76573e20da
enricmartos <enric.martos@gmail.com>
2020-04-04 19:50:14 +0200

    Add ML naive classifier

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:alpine3.7 
+# FROM python:alpine3.7
+FROM python:3.7.2-slim
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt 
+RUN pip install -r requirements.txt
 EXPOSE 8080
 ENTRYPOINT [ "python" ] 
 CMD [ "app.py" ]
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
+
+from Classifier import Classifier
+
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 8080))
 
+
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    classifier = Classifier()
+    accuracy_score = classifier.predict_breast_cancer()
+    return 'The accuracy score of beast cancer prediction is ' + str(accuracy_score)
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = port, debug = True)
+    app.run(host='0.0.0.0', port=port, debug=True)
